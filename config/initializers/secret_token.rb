fb7cece0b21791d6842c6eab2beef81520221569
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-02 19:49:36 -0500

    Initialization and devise setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EZBrm::Application.config.secret_key_base = '3f039ab2f2f2b3e69c5a8322b737924da66eecf6765c45d56689ed3e43c420f2da201a075d78831f634bf5b2c9860536ac9bc6fb7b9efb34eabab7f6c6a8aba8'
