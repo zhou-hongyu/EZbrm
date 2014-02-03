9e044c6b6b89cc2f97a0b4c40daa45e73ece4858
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-02 21:08:46 -0500

    all the controller created

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,10 +1,24 @@
 class CompetitionsController < ApplicationController
   def update
+  	competition = Competition.find(params[:id])
+  	competition.update_attributes(competition: competition_params[:description])
+  	redirect_to :back
   end
 
   def create
+  	Competition.create!(competition_params)
+  	redirect_to :back
   end
 
   def destroy
+  	Competition.find(params[:id]).destroy!
+  	redirect_to :back
+
+  end
+
+  private
+
+  def competition_params
+  	params.require(competition).permit(:startup_id, :store_id, :description)
   end
 end
--- app/controllers/cooperations_controller.rb
+++ app/controllers/cooperations_controller.rb
@@ -1,10 +1,25 @@
 class CooperationsController < ApplicationController
   def update
+  	cooperation = Cooperation.find(params[:id])
+  	cooperation.update_attributes(description: cooperation_params[:description])
+  	redirect_to :back
   end
 
   def create
+  	Cooperation.create!(cooperation_params)
+  	redirect_to :back
   end
 
   def destroy
+  	Cooperation.find(params[:id]).destroy!
+  	redirect_to :back
+  end
+
+  private 
+
+  def cooperation_params
+
+  	params.require(:cooperation).permit(:startup_id, :store_id, :description)
+
   end
 end
